e90b3c59dd3271450ee32538235b93396f023cbf
Panagiotis Dimopoulos <panosdim@gmail.com>
2024-05-08 15:21:41 +0300

    Upgrade to firebase bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.database)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.crashlytics)
